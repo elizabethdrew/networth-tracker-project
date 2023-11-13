bedc4d7663de0fd4e0a7ae346c9bf47ac6f0116d
dreweb <elizabeth.drew@coutts.com>
2023-11-13 12:35:09 +0000

    User service configs load correctly

--- configserver/src/main/java/com/drew/networth/ConfigserverApplication.java
+++ configserver/src/main/java/com/drew/networth/ConfigserverApplication.java
@@ -6,10 +6,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServerApplication {
+public class ConfigserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
-	}
+	public static void main(String[] args) {SpringApplication.run(ConfigserverApplication.class, args);}
 
 }
