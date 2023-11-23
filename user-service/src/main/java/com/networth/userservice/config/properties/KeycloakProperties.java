592633f1d52e18c4184cf5d676e6826b05378f11
dreweb <elizabeth.drew@coutts.com>
2023-11-23 11:51:30 +0000

    Added Not Empty

--- user-service/src/main/java/com/networth/userservice/config/properties/KeycloakProperties.java
+++ user-service/src/main/java/com/networth/userservice/config/properties/KeycloakProperties.java
@@ -1,36 +1,52 @@
 package com.networth.userservice.config.properties;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
 @Configuration
 @ConfigurationProperties(prefix = "keycloak")
-@Getter
-@Setter
+@Data
+@Validated
 public class KeycloakProperties {
+
+
+    @NotEmpty
     private String baseUri;
+    @NotEmpty
     private String logoutRedirectUrl;
+
+    @Valid
     private KeyAdmin keyAdmin = new KeyAdmin();
+
+    @Valid
     private KeyUser keyUser = new KeyUser();
 
-    @Getter
-    @Setter
+    @Data
     public static class KeyAdmin {
+
+        @NotEmpty
         private String realm;
+        @NotEmpty
         private String clientId;
+        @NotEmpty
         private String username;
+        @NotEmpty
         private String password;
     }
 
-    @Getter
-    @Setter
+    @Data
     public static class KeyUser {
+
+        @NotEmpty
         private String realm;
+        @NotEmpty
         private String clientId;
+        @NotEmpty
         private String clientSecret;
     }
-
 
 }
