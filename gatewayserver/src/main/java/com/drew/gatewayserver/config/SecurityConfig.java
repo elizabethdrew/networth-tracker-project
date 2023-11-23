f08d9d0789bcac89d4b5d0268deb654b209fe643
dreweb <elizabeth.drew@coutts.com>
2023-11-23 16:35:07 +0000

    Introspection works

--- gatewayserver/src/main/java/com/drew/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/src/main/java/com/drew/gatewayserver/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.introspection.NimbusOpaqueTokenIntrospector;
+import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -19,8 +21,14 @@ public class SecurityConfig {
                         .pathMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll() // Allow User Login
                         .anyExchange().authenticated()
                 )
-                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
-        http.csrf(csrf -> csrf.disable());
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .opaqueToken(opaqueToken -> opaqueToken
+                                .introspectionUri("http://keycloak:8080/realms/networth/protocol/openid-connect/token/introspect")
+                                .introspectionClientCredentials("apigateway", "Ow1dayvip5w4BHJeacRzVLHLCfJNCm3W")
+                        )
+                )
+        .csrf(csrf -> csrf.disable());
         return http.build();
     }
+
 }
