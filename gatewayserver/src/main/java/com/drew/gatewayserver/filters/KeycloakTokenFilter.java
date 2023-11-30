1ac0907499b9f7720391e8bc379ac275d3c46b9c
dreweb <elizabeth.drew@coutts.com>
2023-11-30 13:01:49 +0000

    Refactored code to return early if ifs fail

--- gatewayserver/src/main/java/com/drew/gatewayserver/filters/KeycloakTokenFilter.java
+++ gatewayserver/src/main/java/com/drew/gatewayserver/filters/KeycloakTokenFilter.java
@@ -21,37 +21,44 @@ public class KeycloakTokenFilter implements GlobalFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        log.info("starting token filter");
+        log.info("Starting token filter");
         List<String> authHeaders = exchange.getRequest().getHeaders().get("Authorization");
-        if (authHeaders != null && !authHeaders.isEmpty()) {
-            String authHeader = authHeaders.get(0);
-            if (authHeader.startsWith("Bearer ")) {
-                String token = authHeader.substring(7);
-                String uid = extractUidFromToken(token);
-                if (uid != null) {
-                    log.info("Added X-User-ID header: " + uid);
-                    ServerHttpRequest mutatedRequest = exchange.getRequest().mutate()
-                            .header("X-User-ID", uid)
-                            .build();
-                    return chain.filter(exchange.mutate().request(mutatedRequest).build());
-                }
-            }
+
+        if (authHeaders == null || authHeaders.isEmpty()) {
+            return chain.filter(exchange);
         }
-        return chain.filter(exchange);
+
+        String authHeader = authHeaders.get(0);
+        if (!authHeader.startsWith("Bearer ")) {
+            return chain.filter(exchange);
+        }
+
+        String token = authHeader.substring(7);
+        String uid = extractUidFromToken(token);
+        if (uid == null) {
+            return chain.filter(exchange);
+        }
+
+        log.info("Added X-User-ID header: " + uid);
+        ServerHttpRequest mutatedRequest = exchange.getRequest().mutate()
+                .header("X-User-ID", uid)
+                .build();
+        return chain.filter(exchange.mutate().request(mutatedRequest).build());
     }
 
     private String extractUidFromToken(String token) {
         try {
             String[] parts = token.split("\\.");
-            if (parts.length < 2) {return null;}
+            if (parts.length < 2) {
+                return null;
+            }
             String payload = parts[1];
             String decodedJson = new String(Base64Utils.decodeFromString(payload));
             ObjectMapper objectMapper = new ObjectMapper();
             Map<String, Object> claims = objectMapper.readValue(decodedJson, Map.class);
-            String sub = claims.get("sub").toString();
-            return sub;
+            return claims.get("sub").toString();
         } catch (IOException e) {
-            log.error("Can't Extract Uid From Token");
+            log.error("Can't Extract Uid From Token", e);
             return null;
         }
     }
