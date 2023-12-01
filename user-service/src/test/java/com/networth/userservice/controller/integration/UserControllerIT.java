7bc0a417e11b0393a64cc7cfacb80c3858a61ccd
dreweb <elizabeth.drew@coutts.com>
2023-12-01 13:31:02 +0000

    tests

--- user-service/src/test/java/com/networth/userservice/controller/integration/UserControllerIT.java
+++ user-service/src/test/java/com/networth/userservice/controller/integration/UserControllerIT.java
@@ -1,21 +1,34 @@
 package com.networth.userservice.controller.integration;
 
-import com.networth.userservice.GlobalTestContainer;
+import com.networth.userservice.MysqlTestContainer;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.annotation.DirtiesContext;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 
-public class UserControllerIT extends GlobalTestContainer {
+public class UserControllerIT extends MysqlTestContainer {
+
+    @Test
+    void testUserLogin() throws Exception {
+        String body = "{ \"username\": \"seeduser\",\n" +
+                "    \"password\": \"Password123!\",\n" +
+                "   }";
+        given().log().all().contentType(ContentType.JSON)
+                .body(body)
+                .when().request("GET", "/api/v1/users/")
+                .then()
+                .statusCode(200)
+                .body("username", is("seeduser"),
+                        "id", is(1));
+    }
 
     @Test
     void testCreateUser() throws Exception {
 
-        String body = "{ \"username\": \"janedoe\",\n" +
-                "    \"email\": \"janedoe@example.co.uk\",\n" +
+        String body = "{ \"username\": \"testuser\",\n" +
+                "    \"email\": \"testuser@example.co.uk\",\n" +
                 "    \"password\": \"Password123!\",\n" +
                 "    \"taxRate\": \"BASIC\",\n" +
                 "    \"dateOfBirth\": \"1990-01-01\"\n" +
@@ -28,7 +41,7 @@ public class UserControllerIT extends GlobalTestContainer {
                 .statusCode(201)
                 .body(
                         "userId", notNullValue(),
-                        "username", is("janedoe"),
+                        "username", is("testuser"),
                         "taxRate", is("BASIC"),
                         "activeUser", is(true)
                 );
