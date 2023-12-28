7582a8a725552b96e34ed9ffea3b6c53a1082c37
dreweb <elizabeth.drew@coutts.com>
2023-12-28 10:09:10 +0000

    Add getBalanceById

--- account-service/src/main/java/com/drew/accountservice/dto/BalanceHistoryDto.java
+++ account-service/src/main/java/com/drew/accountservice/dto/BalanceHistoryDto.java
@@ -1,20 +1,10 @@
 package com.drew.accountservice.dto;
 
 import com.drew.accountservice.entity.Balance;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
-public class BalanceHistoryDto {
-    @JsonProperty("balances")
-    private List<Balance> balances;
-
-    public List<Balance> getBalances() {
-        return balances;
-    }
-
-    public void setBalances(List<Balance> balances) {
-        this.balances = balances;
-    }
-
+public record BalanceHistoryDto (
+        List<Balance> balances
+) {
 }
